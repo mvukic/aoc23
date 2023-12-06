437b39c7a1641965a7be588b01ebd44744d56010
mvukic <matija.vukic2@gmail.com>
2023-12-06 19:58:19 +0100

    day 6

--- src/main/kotlin/day6/part1.kt
+++ src/main/kotlin/day6/part1.kt
@@ -4,4 +4,28 @@ import lines
 
 fun main() {
     val lines = lines("day6")
+
+    val times = lines[0]
+        .split("Time:")[1]
+            .split(" ")
+            .filterNot { it.isBlank() }
+            .map { it.trim().toInt() }
+    val distances = lines[1]
+        .split("Distance:")[1]
+        .split(" ")
+        .filterNot { it.isBlank() }
+        .map { it.trim().toInt() }
+    var mainCounter = 1
+    for ((time, distance) in times.zip(distances)) {
+        println("$time $distance")
+        var counter = 0
+        for (holdTime in (1..< time)) {
+            if((time - holdTime) * holdTime > distance) {
+                counter++
+            }
+        }
+        mainCounter *= counter
+        println("\t Count: $counter")
+    }
+    println("Part 1: $mainCounter")
 }
--- src/main/kotlin/day6/part2.kt
+++ src/main/kotlin/day6/part2.kt
@@ -4,4 +4,20 @@ import lines
 
 fun main() {
     val lines = lines("day6")
+
+    val time = lines[0]
+        .split("Time:")[1]
+        .replace(" ", "").toLong()
+    val distance = lines[1]
+        .split("Distance:")[1]
+        .replace(" ", "").toLong()
+    var counter = 0
+    println("$time $distance")
+    for (holdTime in (1..< time)) {
+        if((time - holdTime) * holdTime > distance) {
+            counter++
+        }
+    }
+    println("\t Count: $counter")
+    println("Part 2: $counter")
 }
