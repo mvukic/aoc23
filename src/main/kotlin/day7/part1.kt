f81c4bca299b302caaf4ef22ab50230881a778d7
mvukic <matija.vukic2@gmail.com>
2023-12-07 21:14:27 +0100

    day 6

--- src/main/kotlin/day7/part1.kt
+++ src/main/kotlin/day7/part1.kt
@@ -6,7 +6,7 @@ fun main() {
     val lines = lines("day7")
     val hands = lines
         .map { line -> line.split(" ").filterNot { it.isBlank() } }
-        .map { (hand, bid) -> Hand.fromString(hand, bid) }
+        .map { (hand, bid) -> HandPart1.fromString(hand, bid) }
 
     val sorted = hands.sortedWith(
         compareBy({ it.typeOrder() },
@@ -24,3 +24,85 @@ fun main() {
     println("Part 1: $result")
 }
 
+
+data class CardPart1(
+    var label: String
+) : Comparable<CardPart1> {
+    override fun compareTo(other: CardPart1): Int {
+        if (label == other.label) {
+            return 0
+        }
+        if (ordersPart1.indexOf(label) > ordersPart1.indexOf(other.label)) {
+            return 1
+        }
+        return -1
+    }
+}
+
+val ordersPart1 = listOf("A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2")
+
+data class HandPart1(
+    val bid: Int,
+    val cards: List<CardPart1>
+) : Comparable<HandPart2> {
+
+    override fun compareTo(other: HandPart2): Int {
+        if (typeOrder() == other.typeOrder()) {
+            return 0
+        }
+        if (typeOrder() < other.typeOrder()) {
+            return 1
+        }
+        return -1
+    }
+
+    fun typeOrder(): Int {
+        val occurrences = cards.groupingBy { it.label }.eachCount()
+        return when {
+            occurrences.values.contains(5) -> {
+//                println("Five of kind")
+                0
+            }
+
+            occurrences.values.contains(4) -> {
+//                println("Four of kind")
+                1
+            }
+
+            occurrences.values.contains(3) && occurrences.values.contains(2) -> {
+//                println("Full house")
+                2
+            }
+
+            occurrences.values.contains(3) && occurrences.values.contains(1) -> {
+//                println("Three of kind")
+                3
+            }
+
+            occurrences.values.contains(2) && occurrences.keys.size == 3 -> {
+//                println("Two pair")
+                4
+            }
+
+            occurrences.values.contains(2) && occurrences.keys.size == 4 -> {
+//                println("One pair")
+                5
+            }
+
+            occurrences.keys.size == 5 -> {
+//                println("High card")
+                6
+            }
+
+            else -> error("WTF!")
+        }
+    }
+
+    companion object {
+        fun fromString(line: String, bid: String) = HandPart1(
+            bid = bid.toInt(),
+            cards = line.split("").filterNot { it.isBlank() }.map { CardPart1(it) }
+        )
+
+    }
+}
--- src/main/kotlin/day7/part2.kt
+++ src/main/kotlin/day7/part2.kt
@@ -6,7 +6,27 @@ fun main() {
     val lines = lines("day7")
     val hands = lines
         .map { line -> line.split(" ").filterNot { it.isBlank() } }
-        .map { (hand, bid) -> Hand.fromString(hand, bid) }
+        .map { (hand, bid) -> HandPart2.fromString(hand, bid) }
+
+    for (hand in hands) {
+        if (!hand.cards.contains(CardPart2("J"))) {
+            continue
+        }
+        var orderType = 6
+        var sign = "J"
+        for (cardSign in ordersPart2) {
+            val copy = hand.copy(cards = hand.cards.map { it.copy() })
+            copy.cards.filter { it.label == "J" }.forEach { it.label = cardSign }
+            if (copy.typeOrder() < orderType) {
+                orderType = copy.typeOrder()
+                sign = cardSign
+            }
+        }
+        hand.cards.filter { it.label == "J" }.forEach {
+            it.label = sign
+            it.wasWildcard = true
+        }
+    }
 
     val sorted = hands.sortedWith(
         compareBy({ it.typeOrder() },
@@ -18,8 +38,91 @@ fun main() {
     )
 
     for (hand in sorted.reversed()) {
+        print("${hand.typeOrder()} ")
         println(hand)
     }
     val result = sorted.reversed().mapIndexed { i, hand -> hand.bid * (i + 1) }.sum()
-    println("Part 1: $result")
+    println("Part 2: $result")
 }
+
+
+data class CardPart2(
+    var label: String,
+    var wasWildcard: Boolean = false
+) : Comparable<CardPart2> {
+
+    fun getRealCard() = if (wasWildcard) "J" else label
+
+    override fun compareTo(other: CardPart2): Int {
+        if (getRealCard() == other.getRealCard()) {
+            return 0
+        }
+        if (ordersPart2.indexOf(getRealCard()) > ordersPart2.indexOf(other.getRealCard())) {
+            return  1
+        }
+        return -1
+    }
+
+    override fun toString(): String {
+        return " $label ($wasWildcard) "
+    }
+}
+
+data class HandPart2(
+    val bid: Int,
+    val cards: List<CardPart2>
+) {
+
+    fun typeOrder(): Int {
+        val occurrences = cards.groupingBy { it.label }.eachCount()
+        return when {
+            occurrences.values.contains(5) -> {
+//                println("Five of kind")
+                0
+            }
+
+            occurrences.values.contains(4) -> {
+//                println("Four of kind")
+                1
+            }
+
+            occurrences.values.contains(3) && occurrences.values.contains(2) -> {
+//                println("Full house")
+                2
+            }
+
+            occurrences.values.contains(3) && occurrences.values.contains(1) -> {
+//                println("Three of kind")
+                3
+            }
+
+            occurrences.values.contains(2) && occurrences.keys.size == 3 -> {
+//                println("Two pair")
+                4
+            }
+
+            occurrences.values.contains(2) && occurrences.keys.size == 4 -> {
+//                println("One pair")
+                5
+            }
+
+            occurrences.keys.size == 5 -> {
+//                println("High card")
+                6
+            }
+
+            else -> error("WTF!")
+        }
+    }
+
+    companion object {
+        fun fromString(line: String, bid: String) = HandPart2(
+            bid = bid.toInt(),
+            cards = line.split("").filterNot { it.isBlank() }.map { CardPart2(it) }
+        )
+
+    }
+
+}
+
+val ordersPart2 = listOf("A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J")
