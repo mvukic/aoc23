2d400a00c57601a738bbd23c84faa7089b4b856b
mvukic <matija.vukic2@gmail.com>
2023-12-11 22:16:47 +0100

    day 11

---
+++ src/main/kotlin/day11/part1.kt
@@ -0,0 +1,79 @@
+package day11
+
+import lines
+
+fun main() {
+    val lines = lines("day11")
+    val expanded = expand1(lines)
+//    printMap(expanded)
+    val points = findPoints1(expanded)
+//    println(points)
+    var sum = 0
+    for (point1Index in points.indices) {
+        for (point2Index in (point1Index..<points.size)) {
+            sum += getDistance1(points[point1Index], points[point2Index])
+        }
+    }
+    println("Part 1: $sum")
+}
+
+fun getDistance1(point1: Point1, point2: Point1): Int {
+    val xDiff = kotlin.math.abs(point2.x - point1.x)
+    val yDiff = kotlin.math.abs(point2.y - point1.y)
+    val d = xDiff + yDiff
+    return d
+}
+
+fun findPoints1(lines: List<String>): List<Point1> {
+    val points = mutableListOf<Point1>()
+    lines.forEachIndexed { rowIndex, row ->
+        row.forEachIndexed { columnIndex, column ->
+            if (column == '#') {
+                points.add(Point1(x = columnIndex, y = rowIndex))
+            }
+        }
+    }
+    return points
+
+}
+
+fun expand1(lines: List<String>): List<String> {
+    val result = mutableListOf<String>()
+
+    for (row in lines) {
+        result.add(row)
+        if (row.all { it == '.' }) {
+            result.add(row)
+        }
+    }
+
+    var expandedRows = 0
+    for (columnIndex in (0..<lines.first().length)) {
+        val column = lines.map { it[columnIndex] }
+        if (column.all { it == '.' }) {
+            expandedRows++
+            result.forEachIndexed { rowIndex, _ ->
+                val row = result[rowIndex].split("").filterNot { it.isBlank() }.toMutableList()
+                row.add(columnIndex + expandedRows, ".")
+                result[rowIndex] = row.joinToString("")
+            }
+        }
+    }
+    return result
+}
+
+fun printMap1(lines: List<String>) {
+    for (line in lines) {
+        for (col in line) {
+            print(col)
+        }
+        println()
+    }
+}
+
+data class Point1(
+    val x: Int,
+    val y: Int
+) {
+    override fun toString() = "(${x}, ${y})"
+}
